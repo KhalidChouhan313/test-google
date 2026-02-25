ac5a69bf072c52fdca0865e6ef11e29d450fe379
khalid chouhan <khalidchuhan7886@gmail.com>
2026-02-25 12:26:47 +0500

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,7 +10,14 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('my-app');
   goToGoogle() {
-    // Forcefully redirect Google
-    window.location.href = 'https://www.google.com';
+    const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
+
+    if (/android/i.test(userAgent)) {
+      window.location.href = 'intent://www.google.com/#Intent;scheme=https;package=com.android.chrome;end';
+    } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+      window.location.href = 'https://www.google.com';
+    } else {
+      window.location.href = 'https://www.google.com';
+    }
   }
 }
